ad386168beeb5224bd36ca2e2b61856764a3ea32
ismailmtioui <44583993+ismailmtioui@users.noreply.github.com>
2024-08-23 17:41:53 +0200

    Delete src/main/java/com/_DSF/je/Repository/VideoRepository.java

--- src/main/java/com/_DSF/je/Repository/VideoRepository.java
+++
@@ -1,9 +0,0 @@
-package com._DSF.je.Repository;
-
-import com._DSF.je.Entity.Video;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VideoRepository extends JpaRepository<Video, Long> {
-}
