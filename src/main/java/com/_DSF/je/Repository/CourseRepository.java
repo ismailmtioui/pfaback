2e7a62e5fb919149b985eb3435a60fac317b365f
ismailmtioui <44583993+ismailmtioui@users.noreply.github.com>
2024-08-23 17:43:22 +0200

    Delete src/main/java/com/_DSF/je/Repository/CourseRepository.java

--- src/main/java/com/_DSF/je/Repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com._DSF.je.Repository;
-
-import com._DSF.je.Entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
